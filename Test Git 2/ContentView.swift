24dc6543758be4460524dfde5c9064e3ba6e084a
DH <hodgesd@gmail.com>
2019-12-07 17:21:09 -0600

    Initial Commit

---
+++ Test Git 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Git 2
+//
+//  Created by Derrick Hodges on 12/7/19.
+//  Copyright © 2019 Derrick Hodges. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
